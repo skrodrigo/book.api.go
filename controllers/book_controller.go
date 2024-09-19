44fea1db20928087083cf457cc0f78c4052fddbd
Rodrigo Carvalho <rodrigoa0987@gmail.com>
2024-09-19 20:26:05 -0300

    update

--- controllers/book_controller.go
+++ controllers/book_controller.go
@@ -1,58 +1,59 @@
 package controllers
 
 import (
-    "github.com/gin-gonic/gin"
-    "net/http"
-    "github.com/spaaws/book-api/config"
-    "github.com/spaaws/book-api/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/skrodrigo/book.api.go/config"
+	"github.com/skrodrigo/book.api.go/models"
 )
 
 func GetBooks(c *gin.Context) {
-    var books []models.Book
-    config.DB.Find(&books)
-    c.JSON(http.StatusOK, books)
+	var books []models.Book
+	config.DB.Find(&books)
+	c.JSON(http.StatusOK, books)
 }
 
 func GetBook(c *gin.Context) {
-    id := c.Param("id")
-    var book models.Book
-    if err := config.DB.First(&book, id).Error; err != nil {
-        c.JSON(http.StatusNotFound, gin.H{"error": "Livro não encontrado!"})
-        return
-    }
-    c.JSON(http.StatusOK, book)
+	id := c.Param("id")
+	var book models.Book
+	if err := config.DB.First(&book, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Livro não encontrado!"})
+		return
+	}
+	c.JSON(http.StatusOK, book)
 }
 
 func CreateBook(c *gin.Context) {
-    var book models.Book
-    if err := c.ShouldBindJSON(&book); err != nil {
-        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-        return
-    }
-    config.DB.Create(&book)
-    c.JSON(http.StatusCreated, book)
+	var book models.Book
+	if err := c.ShouldBindJSON(&book); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	config.DB.Create(&book)
+	c.JSON(http.StatusCreated, book)
 }
 
 func UpdateBook(c *gin.Context) {
-    id := c.Param("id")
-    var book models.Book
-    if err := config.DB.First(&book, id).Error; err != nil {
-        c.JSON(http.StatusNotFound, gin.H{"error": "Livro não encontrado!"})
-        return
-    }
-    if err := c.ShouldBindJSON(&book); err != nil {
-        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-        return
-    }
-    config.DB.Save(&book)
-    c.JSON(http.StatusOK, book)
+	id := c.Param("id")
+	var book models.Book
+	if err := config.DB.First(&book, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Livro não encontrado!"})
+		return
+	}
+	if err := c.ShouldBindJSON(&book); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	config.DB.Save(&book)
+	c.JSON(http.StatusOK, book)
 }
 
 func DeleteBook(c *gin.Context) {
-    id := c.Param("id")
-    if err := config.DB.Delete(&models.Book{}, id).Error; err != nil {
-        c.JSON(http.StatusNotFound, gin.H{"error": "Livro não encontrado!"})
-        return
-    }
-    c.JSON(http.StatusOK, gin.H{"message": "Livro deletado com sucesso!"})
+	id := c.Param("id")
+	if err := config.DB.Delete(&models.Book{}, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Livro não encontrado!"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Livro deletado com sucesso!"})
 }
--- routes/routes.go
+++ routes/routes.go
@@ -1,15 +1,15 @@
 package routes
 
 import (
-    "github.com/gin-gonic/gin"
-    "github.com/spaaws/book-api/controllers"
+	"github.com/gin-gonic/gin"
+	"github.com/skrodrigo/book.api.go/controllers"
 )
 
 func SetupRoutes(r *gin.Engine) {
-    // Define as rotas
-    r.GET("/books", controllers.GetBooks)
-    r.GET("/books/:id", controllers.GetBooks)
-    r.POST("/books", controllers.CreateBook)
-    r.PUT("/books/:id", controllers.UpdateBook)
-    r.DELETE("/books/:id", controllers.DeleteBook)
+	// Define as rotas
+	r.GET("/books", controllers.GetBooks)
+	r.GET("/books/:id", controllers.GetBooks)
+	r.POST("/books", controllers.CreateBook)
+	r.PUT("/books/:id", controllers.UpdateBook)
+	r.DELETE("/books/:id", controllers.DeleteBook)
 }
